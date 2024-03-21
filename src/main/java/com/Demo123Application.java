faf7ba9f3e40f5a8f0a6e2d1f2468ab127ce8e95
unknown <ashubackup>
2024-03-21 18:13:05 +0530

    demo

--- src/main/java/com/Demo123Application.java
+++ src/main/java/com/Demo123Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import ch.qos.logback.classic.Logger;
+
 @SpringBootApplication
 @EnableScheduling
-public class Demo123Application extends SpringBootServletInitializer {
+public class Demo123Application {
 
+//	public static final Logger LOGGER = 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo123Application.class, args);
 	}
